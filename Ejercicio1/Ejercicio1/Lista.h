0167a4173545470da1c6d3b479ed96682a73dc2b
Isabel Galeano <isagaleano9@gmail.com>
2021-06-18 22:53:33 -0600

    Update Ejercicio1

--- Ejercicio1/Ejercicio1/Lista.h
+++ Ejercicio1/Ejercicio1/Lista.h
@@ -2,16 +2,25 @@
 #ifndef LISTA_H
 #define LISTA_H
 #include "Nodo.h"
+#include <string>
 class Lista
 {
 public:
 	Lista();
 	void setCabeza(Nodo *cabeza);
 	Nodo* getCabeza();
+	void setCabeza(int _size);
 	void agregarAlInicio(int data);
-	void MostrarLista();
+	void agregarOrdenado(int data);
+	void mostrarLista();
+	void buscar(int _data);
+	bool esVacia();
+	void eliminar();
+	void eliminarElemento(int pos);
+	int size = 0;
 private:
 	Nodo *cabeza;
+	
 };
 
 #endif // LISTA_H
